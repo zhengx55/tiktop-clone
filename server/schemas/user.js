327d2164af8634fa0cbd2704a72046e94a7375bb
zhengx55 <williamzhxin@gmail.com>
2022-08-03 13:57:01 +0800

    project setup

---
+++ server/schemas/user.js
@@ -0,0 +1,9 @@
+export default {
+  name: "user",
+  title: "User",
+  type: "document",
+  fileds: [
+    { name: "userName", title: "User Name", type: "string" },
+    { name: "image", title: "Image", type: "strign" },
+  ],
+};
