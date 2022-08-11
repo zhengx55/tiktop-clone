967670c5250d5d368af3f643aa1d69f5723bab3c
zhengx55 <williamzhxin@gmail.com>
2022-08-11 14:23:20 +0800

    suggested accounts component

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
-
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect } from 'react'
+import { GoVerified } from 'react-icons/go'
+import useAuthStore from '../store/authStore'
+import { IUser } from '../types'
 const SuggestedAccounts = () => {
-  return <div>SuggestedAccounts</div>
+  const { fetchAllUsers, allUsers } = useAuthStore()
+  useEffect(() => {
+    fetchAllUsers()
+  }, [fetchAllUsers])
+  return (
+    <div className="border-gray=200 pb-4 xl:border-b-2">
+      <p className="m-3 mt-4 hidden font-semibold text-gray-500 xl:block">
+        Suggested Account
+      </p>
+      <div>
+        {allUsers.slice(0, 6).map((user: IUser) => {
+          return (
+            <Link href={`/profile/${user._id}`} key={user._id}>
+              <div className="flex cursor-pointer gap-3 rounded p-2 font-semibold hover:bg-primary">
+                <div className="h-8 w-8">
+                  <Image
+                    src={user.image}
+                    width={34}
+                    height={34}
+                    className="rounded-full"
+                    alt="user profile"
+                    layout="responsive"
+                  />
+                </div>
+                <div className="hidden xl:block">
+                  <p className="text-md flex items-center gap-1 font-bold text-primary">
+                    {user.userName.replaceAll(' ', '')}
+                    <GoVerified className="text-blue-400" />
+                  </p>
+                  <p className="text-xs capitalize text-gray-400">
+                    {user.userName}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default SuggestedAccounts
--- store/authStore.ts
+++ store/authStore.ts
@@ -1,10 +1,18 @@
+import axios from 'axios'
 import create from 'zustand'
 import { persist } from 'zustand/middleware'
 
 const authStore = (set: any) => ({
   userProfile: null,
+  allUsers: [],
   addUser: (user: any) => set({ userProfile: user }),
   removeUser: () => set({ userProfile: null }),
+  fetchAllUsers: async () => {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/users`
+    )
+    set({ allUsers: response.data })
+  },
 })
 
 const useAuthStore = create(
